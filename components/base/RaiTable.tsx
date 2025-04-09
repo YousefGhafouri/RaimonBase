e3411539a5d707542419c3211e0cad69872c4a52
yousef <y.ghafouri@gmail.com>
2025-04-09 17:30:25 +0330

    fix articles and scores.

--- components/base/RaiTable.tsx
+++ components/base/RaiTable.tsx
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Box, CircularProgress, Typography } from '@mui/material';
-import { basicNullHandler } from '../../helpers/table';
+import { basicNullHandler } from '@Base/helpers/table';
 
 interface IheaderItem<T extends object>{
     label: string;
--- helpers/analyzeSeo.ts
+++ helpers/analyzeSeo.ts
@@ -2,42 +2,36 @@
 import { AnalysisWorkerWrapper, Paper as seoPaper } from 'yoastseo';
 import Worker from './seoWorker?worker';
 const worker = new AnalysisWorkerWrapper(new Worker());
+worker.initialize({
+    logLevel: 'TRACE', // Optional, see https://github.com/pimterry/loglevel#documentation
+})
+
 
 const defaultAttributes = {
-    keyword: "",
-    synonyms: "",
-    description: "",
-    title: "",
+    keyword: '',
+    synonyms: '',
+    description: '',
+    title: '',
     titleWidth: 0,
-    slug: "",
-    locale: "en_US",
-    permalink: "",
-    date: "",
+    slug: '',
+    locale: 'fa_IR',
+    permalink: '',
+    date: '',
     customData: {},
-    textTitle: "",
-    writingDirection: "LTR",
+    textTitle: '',
+    writingDirection: 'LTR',
     wpBlocks: [],
-    isFrontPage: false
-  };
+    isFrontPage: false,
+};
 
-export const analyzeSEO = async ({text,attributes=defaultAttributes}: any) => {
-  return worker
-      .initialize({
-          logLevel: 'TRACE', // Optional, see https://github.com/pimterry/loglevel#documentation
-      })
-      .then(() => {
-          // The worker has been configured, we can now analyze a Paper.
-          const paper = new seoPaper(text, attributes);
-
-          return worker.analyze(paper);
-      })
-      .then((results: any) => {
-          console.log('Analysis results:');
-          console.log(results);
-          return results
-      })
-      .catch((error: any) => {
-          console.error('An error occured while analyzing the text:');
-          console.error(error);
-      });
+export const analyzeSEO = async ({
+    text,
+    attributes = defaultAttributes,
+}: any) => {
+    const paper = new seoPaper(text, attributes);
+  
+    // const paper = new seoPaper(text, attributes);
+    
+    const data = worker.analyze(paper);
+    return data
 };
