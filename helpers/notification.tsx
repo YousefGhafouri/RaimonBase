9520dfca305c52f38a30466e761ed002aa08be48
yousef <y.ghafouri@gmail.com>
2025-04-13 12:29:45 +0330

    add server error.

---
+++ helpers/notification.tsx
@@ -0,0 +1,35 @@
+import { toast, ToastOptions } from 'react-toastify';
+
+const options: ToastOptions = {
+    position: 'top-center',
+    autoClose: 5000,
+    pauseOnHover: true,
+    closeOnClick: true,
+    hideProgressBar: false,
+};
+
+function RenderError({ msg }: { msg: string }) {
+    return (
+        <div>
+            {msg.split('|').map((text) => (
+                <div>{text}</div>
+            ))}
+        </div>
+    );
+}
+
+export function $serverError(msg: string, userOptions?: ToastOptions) {
+    toast.error(<RenderError msg={msg} />, { ...options, ...userOptions });
+}
+export function $error(msg: string, userOptions?: ToastOptions) {
+    toast.error(msg, { ...options, ...userOptions });
+}
+export function $info(msg: string, userOptions?: ToastOptions) {
+    toast.info(msg, { ...options, ...userOptions });
+}
+export function $success(msg: string, userOptions?: ToastOptions) {
+    toast.success(msg, { ...options, ...userOptions });
+}
+export function $warning(msg: string, userOptions?: ToastOptions) {
+    toast.warning(msg, { ...options, ...userOptions });
+}
