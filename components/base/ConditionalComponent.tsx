61f97e1c40978396f7d29badc218871a207700ff
yousef <y.ghafouri@gmail.com>
2025-04-22 12:11:27 +0330

    fix conditional Component.

--- components/base/ConditionalComponent.tsx
+++ components/base/ConditionalComponent.tsx
@@ -1,18 +1,38 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-type Props = {
-    condition: boolean;
-    children?: ReactNode;
-    render?: ()=>ReactNode;
+type BaseProps<T extends {} = {}, U extends {} = {}> = {
+  condition: boolean;
+  render?: React.FC<T>;
+  renderProps?: T;
+  primaryProps?: U;
 };
 
-export default function ConditionalComponent({
-    condition,
-    children,
-    render,
-}: Props) {
-    if (condition && render) {
-        return render();
-    }
-    return children;
+type WithPrimary<T extends {} = {}, U extends {} = {}> = BaseProps<T, U> & {
+  primary: React.FC<U>;
+  children?: never;
+};
+
+type WithChildren<T extends {} = {}, U extends {} = {}> = BaseProps<T, U> & {
+  children?: React.FC<U>;
+  primary?: never;
+};
+
+type ConditionalComponentProps<T extends {} = {}, U extends {} = {}> =
+  | WithPrimary<T, U>
+  | WithChildren<T, U>;
+
+export default function ConditionalComponent<
+  T extends {} = {},
+  U extends {} = {}
+>({
+  condition,
+  children,
+  primary: Primary = children,
+  primaryProps,
+  render: Render,
+  renderProps,
+}: ConditionalComponentProps<T, U>) {
+  if (condition && Render) return <Render {...(renderProps ?? ({} as T))} />;
+  if (Primary) return <Primary {...(primaryProps ?? ({} as U))} />;
+  return null;
 }
