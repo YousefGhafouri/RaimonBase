e1f7f78a156f77c58d083a41d664c1e5d4a22bcc
yousef <y.ghafouri@gmail.com>
2025-04-29 17:00:33 +0330

    sm changes.

--- provider/RaiModal.tsx
+++ provider/RaiModal.tsx
@@ -1,10 +1,36 @@
-import { useModal } from '@Base/provider/ModalProvider';
+import { useModal, ModalStateI } from '@Base/provider/ModalProvider';
+import React from 'react';
+
+interface Props<T extends object>{
+  modal:ModalStateI<T>
+  closeModal:(inp?:any)=>void
+}
+
+function RenderModal <T extends object>({modal,closeModal}:Props<T>):React.ReactNode{
+  if(typeof modal.ContentComponent === "function") return <modal.ContentComponent closeModal={closeModal}  {...modal.contentProps} />
+  return modal.ContentComponent
+  // fixme:do sth
+  const childrenWithProps = React.Children.map(modal.ContentComponent, child => {
+    // Checking isValidElement is the safe way and avoids a typescript error too.
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, {
+        closeModal: closeModal,
+        // @ts-ignore
+        ...child.props,
+      });
+    }
+    return child;
+  });
+
+  return <>{childrenWithProps}</>
+  // return modal.ContentComponent
+}
 
 function RaiModal() {
-    const modals = useModal((state) => state.modals);
-    const closeModal = useModal((state) => state.closeModal);
+  const modals = useModal((state) => state.modals);
+  const closeModal = useModal((state) => state.closeModal);
     return modals.map((Modal) => (
-        <Modal.ContentComponent key={Modal.id} closeModal={closeModal(Modal)} {...Modal.contentProps} />
+      <RenderModal key={Modal.id} modal={Modal} closeModal={closeModal(Modal)} />
     ));
 }
 
